6a374ba698f2cd1e43de445cf40ee5d345fea2f6
VniBOT1 <110419673+VniBOT1@users.noreply.github.com>
2022-09-05 15:59:41 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -31,18 +31,18 @@ module.exports = {
         global.img = 'https://telegra.ph/file/0c567eac403737766688f.jpg'
         global.fla = pickRandom(global.flaaa)
         global.namabot = conn.user.name
-        global.packname = '© CUTE IQ-MD'
-        global.author = 'Made By Ziv San © 2022'
+        global.packname = '© VINN™'
+        global.author = '© VINN™ 2022'
         global.packname2 = 'Made With'
-        global.author2 = 'Cute Bot By Ziv San © 2022'
-        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Ziv San˚'
-        global.wm2 = '© CUTE IQ-MD' + ' ' + 'By Ziv San'
-        global.wm = '© CUTE IQ-MD'
+        global.author2 = '© VINN™ 2022'
+        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By VINN™˚'
+        global.wm2 = '© Vinn BOT' + ' ' + 'VINN™'
+        global.wm = '© VINN™'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'By Haori & Ziv San'
+        global.colong2 = 'By Vinn BOT & VINN™'
         global.kontak2 = [
-        ['16199961931', 'Ziv San', 'Creator Bot', 'Zivsan@support.com', true],
-        ['6282179137771', 'CUTE IQ-MD', 'Bot WhatsApp', 'Just Bot', true]
+        ['6281257170601', 'VINN™', 'Creator Bot', 'Zivsan@support.com', true],
+        ['6285828748603', 'Vinn BOT', 'Bot WhatsApp', 'Just Bot', true]
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
