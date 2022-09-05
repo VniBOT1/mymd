66d998584db2889acf4bd1fdd4289ac9b0162213
VniBOT1 <110419673+VniBOT1@users.noreply.github.com>
2022-09-05 16:32:52 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
+│ • Telkomsel [081257170601]
+│ • Gopay  [081257170601]
+│ • Dana  [081257170601]
 │ • Saweria  [https://saweria.co/thesadboy01]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6281257170602
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
